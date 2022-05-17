d3554e15e24eff0ee582004c7f436b544188b098
David Bowland <dbowland1@gmail.com>
2022-05-16 23:18:45 -0500

    Fix runLocal environment variables

--- scripts/runLocal.sh
+++ scripts/runLocal.sh
@@ -16,13 +16,11 @@ sam build --template ${SAM_TEMPLATE}
 
 # Start the API locally
 export CORS_DOMAIN='http://scatter.bowland.link'
-export DYNAMODB_DECISION_TABLE_NAME=scatter-session-decision-test
-export DYNAMODB_SESSION_TABLE_NAME=scatter-session-api-test
+export DYNAMODB_DECISION_TABLE_NAME=scatter-session-api-decisions-test
+export DYNAMODB_SESSION_TABLE_NAME=scatter-session-api-sessions-test
 export ID_MIN_LENGTH=3
 export ID_MAX_LENGTH=4
-export MAPS_API_KEY=$(aws apigateway get-api-key --api-key 7lzyy1kkbj --include-value --region us-east-2 | jq -r .value)
-export MAPS_API_URL='https://scatter-maps-api-internal.bowland.link/v1'
 export SMS_API_KEY=$(aws apigateway get-api-key --api-key l3q9ffyih6 --include-value --region us-east-1 | jq -r .value)
 export SMS_API_URL='https://sms-queue-api.bowland.link/v1'
 export USER_POOL_ID=us-east-2_xqxzyIOz4
-sam local start-api --region=us-east-2 --force-image-build --parameter-overrides "Environment=test MapsApiKey=$MAPS_API_KEY SmsApiKey=$SMS_API_KEY" --log-file local.log
+sam local start-api --region=us-east-2 --force-image-build --parameter-overrides "Environment=test SmsApiKey=$SMS_API_KEY" --log-file local.log
